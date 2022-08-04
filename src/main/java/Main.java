205908c46bd0dfd99e0c263a4fd84f12f4538215
VozunMaxim <vozun.maxim@gmail.com>
2022-08-04 10:47:21 +0300

    third commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,28 @@
 public class Main {
+    public static void main(String[] args) {
+
+        Post post = new Post();
+        post.birthday = new FormDate();
+
+        post.birthday.day = 13;
+        post.birthday.mouth = 6;
+        post.birthday.year = 1999;
+
+        post.surname = "Иванов";
+        post.name = "Иван";
+        post.patronymic = "Иванович";
+        post.passport = "4444 № 44444444";
+        post.phone = "+7 (999)-999-99-99";
+        post.subscription = true;
+
+        System.out.println(post.birthday.day);
+        System.out.println(post.birthday.mouth);
+        System.out.println(post.birthday.year);
+        System.out.println(post.surname);
+        System.out.println(post.name);
+        System.out.println(post.patronymic);
+        System.out.println(post.passport);
+        System.out.println(post.phone);
+        System.out.println(post.subscription);
+    }
 }
